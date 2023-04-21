40c6ad4f988a498287dd3ff0c85f6e169972b451
Alexander Hoch <zancorgithub@zancor.de>
2023-04-21 09:24:58 +0200

    Initial Commit

---
+++ GitHubTest/GitHubTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubTestApp.swift
+//  GitHubTest
+//
+//  Created by Student on 21.04.23.
+//
+
+import SwiftUI
+
+@main
+struct GitHubTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
